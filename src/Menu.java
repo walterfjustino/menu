72abd8194c4cb9a8233fed25a2ade2158337812c
Walter Fernandes Justino <walterfjustino@gmail.com>
2021-03-28 23:17:44 -0300

    update menu

--- src/Menu.java
+++ src/Menu.java
@@ -1,47 +1,67 @@
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Menu {
-    public static void main(String[] args){
-        System.out.println("|---------> MENU DE OPÇÕES: <--------");
-        System.out.println("|      Selecione às opções abaixo:      |");
-        System.out.println("|      1.  Opção: 1   |");
-        System.out.println("|      2.  Opção: 2   |");
-        System.out.println("|      3.  Opção: 3   |");
-        System.out.println("|      4.  Sair       |");
+
+        public static void menu() throws Exception{
+            System.out.println("|---------> MENU DE OPÇÕES: <--------");
+            System.out.println("|      Selecione às opções abaixo:      |");
+            System.out.println("|      1.  Opção: 1   |");
+            System.out.println("|      2.  Opção: 2   |");
+            System.out.println("|      3.  Opção: 3   |");
+            System.out.println("|      4.  Sair       |");
+        }
+
+
+        public static void opcao1(){
+            System.out.println("selecionado Opção: 1 ");
+        }
+        public static void opcao2(){
+            System.out.println("selecionado Opção: 2 ");
+        }
+        
+        public static void opcao3(){
+            System.out.println("selecionado Opção: 3 ");
+        }
+
+        public static void opcao4(){
+            System.out.println(" Opção: 4 -- Sair!");
+        }
+    
+    
+
+    public static void main(String[] args) throws Exception{
+        int opcao; 
+      
 
         Scanner menu = new Scanner(System.in);
-
-        System.out.println(" Selecione uma opção: ");
-       
-        
-        try{
-            int opcao = menu.nextInt();
-            if(opcao == menu.nextInt()){ //IF THE USER ENTERS ANY CHARACTER DIFFERENT FROM 
-                switch (opcao) {         // THE MENU NUMBERS LAUNCHES AN EXCEPTION  
-                case 1:
-                System.out.println("Opção 1 selecionada");
-                    break;
-                case 2:
-                System.out.println("Opção 2 selecionada"); 
-                    break;   
-                case 3:
-                System.out.println("Opção 3 selecionada");
-                    break;
-                case 4:
-                System.out.println("Sair");
-                    break;
-                default:
-                System.out.println("Opção inválida selecionada");
-                    break;
-             
-                } 
-            }
-        }
-                                    
-         catch(Exception e){
-            System.err.println("Opção digitada não confere com às opções do MENU");
-        
+     
+               do{ 
+                   menu();
+                    opcao = menu.nextInt();
+                    switch (opcao) {       
+                        case 1:
+                        opcao1();
+                        break;
+                        case 2:
+                        opcao2();
+                            break;   
+                        case 3:
+                       opcao3();
+                            break;
+                        case 4:
+                        opcao4();
+                            break;  
+                        
+                        default:
+                        if(opcao <= 0 || opcao >= 5 ) 
+                          System.out.println("Opção inválida selecionada"); 
+                            break;
+                    }                     
+           } while(opcao != 4);    
+           menu.close();
         } 
-
     }
-}
+
+
